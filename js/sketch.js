de8f71625d1fdab9865a6ea3ba3d04256be92933
Indy <116882977+indyyd@users.noreply.github.com>
2023-01-10 13:22:35 +0000

    Add files via upload
    done for all main arrow keys

--- js/sketch.js
+++ js/sketch.js
@@ -19,6 +19,55 @@ function setup() {
     polySynth = new p5.PolySynth();
 }
 
+function playSynth1(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('C3', vel, 0, dur);
+    polySynth.play('E3', vel, time += 0, dur);
+    polySynth.play('G3', vel, time += 0, dur);
+}
+
+function playSynth2(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('D3', vel, 0, dur);
+    polySynth.play('F3', vel, time += 0, dur);
+    polySynth.play('A4', vel, time += 0, dur);
+}
+
+function playSynth3(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('E3', vel, 0, dur);
+    polySynth.play('G3', vel, time += 0, dur);
+    polySynth.play('B4', vel, time += 0, dur);
+}
+
+function playSynth4(){
+    userStartAudio();
+
+    var dur = 1.5;
+    var time = 0;
+    var vel = 0.1;
+    
+    polySynth.play('F3', vel, 0, dur);
+    polySynth.play('A4', vel, time += 0, dur);
+    polySynth.play('C4', vel, time += 0, dur);
+
+}
+
 function draw() {
   background(0, 128, 255);
 
@@ -32,8 +81,25 @@ function draw() {
 }
 
 function keyPressed(){
-   if (keyCode === UP_ARROW) {
-  circleX = 250;
-  circleY = 250;
-  circleSize = 0;
-}}
+   if (keyCode === LEFT_ARROW) {
+        circleX = 250;
+        circleY = 250;
+        circleSize = 0;
+        playSynth1();
+    } else if (keyCode === UP_ARROW) {
+        circleX = 50;
+        circleY = 50;
+        circleSize = 0;
+        playSynth2();
+    } else if (keyCode === DOWN_ARROW) {
+        circleX = 450;
+        circleY = 450;
+        circleSize = 0;
+        playSynth3();
+    } else if (keyCode === RIGHT_ARROW) {
+        circleX = 300;
+        circleY = 300;
+        circleSize = 0;
+        playSynth4();
+    }
+}
